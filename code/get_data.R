2bf8f77e6d8616e1740d6cdba7f0966a1adc9bc3
Edgar Bahilo <edgar.foreground@gmail.com>
2020-12-13 10:54:15 +0100

    loading data module

--- code/get_data.R
+++ code/get_data.R
@@ -1,9 +1,22 @@
+#'@author Edgar Bahilo Rodríguez
+#'@param input_path The directory where the input data is stored
+#'@param file_name the file that we want to load
+#'@return A tibble dataframe containing the twitter data
 
-loadData <- function(input_path) {
-  tryCatch(
+
+loadData <- function(input_path, file_name) {
+  data <- tryCatch(
     {
-    
-  }
+      vroom(paste0(input_path, "/", file_name))
+    }, error = function(e){
+      log_error(e)
+      return(NA)
+    }, warning = function(w){
+      log_warn(w)
+      return(NULL)
+    }, finally = {
+      log_info("Reading data succeded")
+    }
   )
 }
 
--- code/main.R
+++ code/main.R
@@ -1,6 +1,35 @@
 library(vroom)
 library(logger)
 library(optparse)
-
+library(dplyr)
+library(tidyr)
+library(tidytext)
 
 log_level(level = "INFO")
+
+source("get_data.R")
+
+### Note, change defaults by environment variables that can be injected to the code if running
+### as a container somewhere
+
+option_list <- list( 
+  make_option(c("-i", "--input_data"), default='../data/input/',
+              help="Default input data directory[default]",
+              metavar="character"),
+  make_option(c("-o", "--output_data"), default='../data/output/',
+              help="Default output data directory[default]",
+              metavar="character"),
+  make_option(c("-f", "--file_name"), default='twitter_dataset_full.csv',
+              help="Default output data directory[default]",
+              metavar="character"),
+  make_option(c("-m", "--model"), default='../model/',
+              help="Model directory",
+              metavar="character")
+)
+
+# get command line options, if help option encountered print help and exit,
+# otherwise if options not found on command line then set defaults, 
+argument_parser <- parse_args(OptionParser(option_list=option_list))
+
+twitter_data <- loadData(input_path = argument_parser$input_data, 
+                         file_name = argument_parser$file_name)
